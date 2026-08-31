85a0a7cfad796b4aafd125e4c3025c688f675c0d
duhfreakinduh <duhfreakinduh@gmail.com>
2026-08-30 21:20:48 -0500

    Refresh cache for TV UI v7

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='quick-launch-tv-v6';
+const CACHE='quick-launch-tv-v7';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
